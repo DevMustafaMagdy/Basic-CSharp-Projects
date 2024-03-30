180fe7851729c8d1702af882227269dace5536e9
Mustafa <m.magdyweb@gmail.com>
2024-03-30 02:45:53 +0200

    Add new project for quiz game

---
+++ Projects.QuizGame/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Projects.QuizGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
